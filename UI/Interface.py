99ca3758d46c72ff2962c7b4e128694b03ef820d
Anca-Elena Perca <anca-elena.perca@student.tuiasi.to>
2021-11-29 21:21:45 +0200

    Interface update

--- UI/Interface.py
+++ UI/Interface.py
@@ -9,24 +9,26 @@ class Base_Window:
     def __init__(self):
         self.window = Tk()
         self.window.wm_title('Browser CoAP')
-        self.window.geometry("400x400")
-        self.button_connect = Button(self.window, text="Connect", width=20, bg='white', fg='black')
-        self.button_connect.place(x=100, y=100)
-        self.connect_text = Label(self.window, text="Conectare la server", width=30, font=("arial", 10, "bold"))
-        self.connect_text.place(x=50, y=50)
+        self.window.geometry("600x400")
+        self.button_connect = Button(self.window, height=3, width=10, text="Connect", font="arial 15 bold", bg="#98fb98")
+        self.button_disconnect = Button(self.window, height=3, width=10, text="Disconnect", font="arial 15 bold", bg="#ff0000")
+        self.button_clearScreen = Button(self.window, height=3, width=10, text="Clear Screen", font="arial 15 bold", bg="#98fb98")
+        self.button_exit = Button(self.window, height=3, width=10, text="Exit", font="arial 15 bold", bg="#98fb98")
 
+        self.button_connect.place(x=450, y=50)
+        self.button_clearScreen.place(x=450, y=150)
+        self.button_exit.place(x=450, y=250)
 
 # class Connect_Window:
 
 #    def __init__(self):
 #        self.window = Tk()
-#        self.window.wm_title('Connecting')
+#        self.window.wm_title('***Connecting to the server***')
 #        self.window.geometry("200x200")
 #        self.server_ip_entry = Entry(self.window, textvar=fn)
 #        self.server_ip_entry.place(x=100, y=50)
 #        self.server_ip_text = Label(self.window, text="Server IP:", width=20, font=("arial", 10, "bold"))
 #        self.server_ip_text.place(x=50, y=50)
-
 
 app = Base_Window()
 app.window.mainloop()
