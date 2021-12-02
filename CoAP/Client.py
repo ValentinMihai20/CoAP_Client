aaef894cd7a6eb8d13f9b4d383a8f80199370185
Valentin Mihai <valentin.mihai@student.tuiasi.ro>
2021-12-02 16:01:01 +0200

    Remodelare client + interfata

--- CoAP/Client.py
+++ CoAP/Client.py
@@ -1,13 +1,41 @@
 import socket
+import threading
+
+import select
 
 
 class Client:
-    def __init__(self, client_port: int, server_port: int, server_ip: str):
-        self.client_port = client_port
-        self.server_port = server_port
-        self.server_ip = server_ip
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    client_socket = None
+    ip = ""  # ip for client and server
+    client_port = 0
+    server_port = 0
+    data = None
+    running = False
+    receive_thread = None
 
-    def connect(self):
-        self.socket.bind(self.server_ip, int(self.client_port))
+    @classmethod
+    def __init__(cls):
+        cls.ip = "127.0.0.1"  # local ip both client and server have
+        cls.client_port = 2000  # peer port
+        cls.server_port = 2001  # my port
+        cls.data = None
 
+        cls.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        cls.client_socket.bind(("127.0.0.1", cls.client_port))
+
+        cls.running = False
+        cls.receive_thread = threading.Thread(target=cls.receive_fct, args=(cls.client_socket,))
+
+    @classmethod
+    def receive_fct(cls):
+        counter = 0
+        while cls.running:
+            # Apelam la functia sistem IO -select- pentru a verifca daca socket-ul are date in bufferul de receptie
+            # Stabilim un timeout de 1 secunda
+            r, _, _ = select.select([cls.client_socket], [], [], 1)
+            if not r:
+                counter = counter + 1
+            else:
+                data, address = cls.client_socket.recvfrom(1024)
+                print("Received ", str(data), " from ", address)
+                print("Counter = ", counter)
--- CoAP/Interface.py
+++ CoAP/Interface.py
@@ -1,34 +1,28 @@
 from tkinter import *
 
 
-# fn = StringVar()
-
-
 class Base_Window:
 
     def __init__(self):
         self.window = Tk()
         self.window.wm_title('Browser CoAP')
         self.window.geometry("600x400")
-        self.button_connect = Button(self.window, height=3, width=10, text="Connect", font="arial 15 bold", bg="#98fb98")
-        self.button_disconnect = Button(self.window, height=3, width=10, text="Disconnect", font="arial 15 bold", bg="#ff0000")
-        self.button_clearScreen = Button(self.window, height=3, width=10, text="Clear Screen", font="arial 15 bold", bg="#98fb98")
-        self.button_exit = Button(self.window, height=3, width=10, text="Exit", font="arial 15 bold", bg="#98fb98")
+        self.button_connect = Button(self.window, height=3, width=10, text="Connect", font="arial 15 bold",
+                                     bg="#98fb98")
+        self.button_disconnect = Button(self.window, height=3, width=10, text="Disconnect", font="arial 15 bold",
+                                        bg="#ff0000")
+        self.button_clearScreen = Button(self.window, height=3, width=10, text="Clear Screen", font="arial 15 bold",
+                                         bg="#98fb98")
+        self.button_exit = Button(self.window, height=3, width=10, text="Exit", font="arial 15 bold",
+                                  bg="#98fb98")
+
+        self.text_box = Text(self.window, height=25, width=66, font="arial 10", bg="white")
+        self.text_box2 = Text(self.window, height=5, width=66, font="arial 10", bg="white")
 
         self.button_connect.place(x=450, y=50)
         self.button_clearScreen.place(x=450, y=150)
         self.button_exit.place(x=450, y=250)
 
-# class Connect_Window:
-
-#    def __init__(self):
-#        self.window = Tk()
-#        self.window.wm_title('***Connecting to the server***')
-#        self.window.geometry("200x200")
-#        self.server_ip_entry = Entry(self.window, textvar=fn)
-#        self.server_ip_entry.place(x=100, y=50)
-#        self.server_ip_text = Label(self.window, text="Server IP:", width=20, font=("arial", 10, "bold"))
-#        self.server_ip_text.place(x=50, y=50)
 
 app = Base_Window()
 app.window.mainloop()
