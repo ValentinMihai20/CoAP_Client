29592a2ec939cfc356eab9a50279cc007193ca05
Anca-Elena Perca <anca-elena.perca@student.tuiasi.to>
2021-12-02 17:03:40 +0200

    Modificare Client.py

--- CoAP/Client.py
+++ CoAP/Client.py
@@ -25,6 +25,38 @@ class Client:
 
         cls.running = False
         cls.receive_thread = threading.Thread(target=cls.receive_fct, args=(cls.client_socket,))
+    @classmethod
+    def client_connect(cls):
+        cls.running = True
+        cls.send_to_server()
+
+    @classmethod
+    def client_disconnect(cls):
+        cls.running = False
+
+    @classmethod
+    def set_data(cls, data):
+        cls.data = data
+
+    @classmethod
+    def send_to_server(cls):
+        try:
+            cls.receive_thread = threading.Thread(target=cls.receive_fct)
+            cls.receive_thread.start()
+        except:
+            print("Error at starting the thread!")
+            return
+
+        while True:
+            data = cls.data
+            if data is not None:
+                cls.client_socket.sendto(bytes(data, encoding="ascii"), (cls.ip, int(cls.client_port)))
+                cls.data = None
+            if not cls.running:
+                print("Waiting for the thread to close.")
+                cls.receive_thread.join()
+                print("Thread receive_thread closed.")
+                break
 
     @classmethod
     def receive_fct(cls):
